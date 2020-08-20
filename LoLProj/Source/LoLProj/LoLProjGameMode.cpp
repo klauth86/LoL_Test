0a3d9e4402c14e95549b1050ffbe0b51a15e6053
Artur Khayrullin <kehel@yandex.ru>
2020-08-20 21:30:51 +0500

    UDEMY_UC_CPP_L239

--- LoLProj/Source/LoLProj/LoLProjGameMode.cpp
+++ LoLProj/Source/LoLProj/LoLProjGameMode.cpp
@@ -9,7 +9,7 @@ ALoLProjGameMode::ALoLProjGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/BP/BP_FPCharacter.BP_FPCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
