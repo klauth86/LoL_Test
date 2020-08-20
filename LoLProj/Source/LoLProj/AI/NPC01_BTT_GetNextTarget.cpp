f729d16674d3d7b2efbecc242d1ba3feb2214d50
Artur Khayrullin <kehel@yandex.ru>
2020-08-20 22:49:35 +0500

    UDEMY_UC_CPP_L244

---
+++ LoLProj/Source/LoLProj/AI/NPC01_BTT_GetNextTarget.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "NPC01_BTT_GetNextTarget.h"
+
+EBTNodeResult::Type UNPC01_BTT_GetNextTarget::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) {
+
+	return EBTNodeResult::Type::Succeeded;
+}
